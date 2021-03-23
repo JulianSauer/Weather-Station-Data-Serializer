0962f25f1db26bdaa864c3a84807b07da57fa94f
Julian Sauer <julian.sauer@adesso.de>
2021-03-23 19:38:41 +0100

    Initial project setup

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+    "context"
+    "encoding/json"
+    "fmt"
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+    "os"
+)
+
+const TABLE = "WeatherStation"
+
+type WeatherData struct {
+    MessageId     string  `json:"messageId,omitempty"`
+    Timestamp     string  `json:"timestamp"`
+    Temperature   float64 `json:"temperature"`
+    Humidity      uint8   `json:"humidity"`
+    WindSpeed     float64 `json:"windSpeed"`
+    GustSpeed     float64 `json:"gustSpeed"`
+    Rain          float64 `json:"rain"`
+    WindDirection float32 `json:"windDirection"`
+}
+
+func handler(ctx context.Context, snsEvent events.SNSEvent) {
+    fmt.Println("Connecting to DynamoDB")
+    session := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+    db := dynamodb.New(session)
+
+    fmt.Printf("Received %d message(s):", len(snsEvent.Records))
+    for _, record := range snsEvent.Records {
+        fmt.Println(record.SNS.Message)
+        d := WeatherData{}
+        e := json.Unmarshal([]byte(record.SNS.Message), &d)
+        handle(e)
+
+        d.MessageId = record.SNS.MessageID
+        put(&d, db)
+    }
+}
+
+func put(data *WeatherData, db *dynamodb.DynamoDB) {
+    attribute, e := dynamodbattribute.MarshalMap(data)
+    handle(e)
+
+    input := &dynamodb.PutItemInput{
+        Item:      attribute,
+        TableName: aws.String(TABLE),
+    }
+
+    _, e = db.PutItem(input)
+    handle(e)
+}
+
+func handle(e error) {
+    if e != nil {
+        fmt.Println(e.Error())
+        os.Exit(1)
+    }
+}
+
+func main() {
+    lambda.Start(handler)
+}
